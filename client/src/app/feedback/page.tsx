cb0060c094104a4c838ae1488a8c53762a5f8067
tusharshaha <tusharshaha128@gmail.com>
2025-01-01 19:58:15 +0600

    add: added error handler on data fetchning

--- client/src/app/feedback/page.tsx
+++ client/src/app/feedback/page.tsx
@@ -3,16 +3,27 @@ import { NEXT_PUBLIC_BACKEND_URL } from '@/config';
 import Link from 'next/link';
 import React from 'react';
 
+async function getFeedback() {
+  try {
+    const res = await fetch(
+      `${NEXT_PUBLIC_BACKEND_URL}/feedback?limit=15&skip=0`
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const Feedback = async () => {
-  const res = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/feedback?limit=15&skip=0`);
-  const data = await res.json();
+  const data = await getFeedback();
   return (
-    <div className='min-h-screen cus-container py-8 text-right'>
+    <div className="min-h-screen cus-container py-8 text-right">
       <Link href="/" className="ghost-btn">
         Back to Home &#8594;
       </Link>
 
-      <Table data={data.data}/>
+      <Table data={data.data} />
     </div>
   );
 };
